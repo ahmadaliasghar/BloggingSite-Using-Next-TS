ebb5f7db4f3f2b85d0589af2b2b8c50108a62672
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-06-03 17:37:36 +0500

    GetServerSideProps

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from "react";
 import styles from "@/styles/Blog.module.css";
 import Link from "next/link";
+import { GetServerSideProps } from "next";
+// import { log } from "console";
 
 type Blog = {
-  slug: string;
   title: string;
   description: string;
+  slug: string;
+  author: string;
 };
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState<Blog[]>([]);
+export  async function getServerSideProps(context: any) {
+  let data = await fetch('http://localhost:3000/api/blogs');
+  let allBlogs = await data.json();
+  // {console.log(allBlogs)}
+  return {
+    props: {allBlogs},
+  };
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/blogs')
-      .then((response) => response.json())
-      .then((data) => setBlogs(data.allBlogs));      
-  }, []);
 
-  // console.log(blogs);
+const Blog = (props: any) => {
+
+  const [blogs, setBlogs] = useState<Blog[]>(props.allBlogs.allBlogs);
+  
   return (
     <div>
-     {blogs.map((blog) => ( 
+      {blogs.map((blog) => (
         <div className={styles.blogs} key={blog.slug}>
           <div className={styles.blogitem}>
             <Link href={`/blogpost/${blog.slug}`}>
@@ -31,7 +38,7 @@ const Blog = () => {
         </div>
       ))}
     </div>
-  );
+    );
 }
 
 export default Blog;
--- src/pages/blogpost/[slug].tsx
+++ src/pages/blogpost/[slug].tsx
@@ -1,48 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/Blogpost.module.css";
 
-const slug = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  console.log(slug);
+interface Blog {
+  slug: string;
+  title: string;
+  description: string;
+  author: string;
+}
+
+
+export  async function getServerSideProps(context: any) {
+  // const router = useRouter();
+  // let allBlogs = await data.json();
+  // {console.log(allBlogs)}
+  const { slug } = context.query;
+  let data =  await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`);
+  let blog = await data.json();
+
+  return {
+    props: {blog},
+  };
+}
+
+const BlogPost = (props: any) => {
+  const [blog, setBlog] = useState<Blog>(props.blog);
+
   return (
-    <>
-      <div className={styles.main}>
-        <h1>Name of the Blog: {slug}</h1>
-        <p className={styles.blogtext}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti rem
-          ad nihil repellat minus non, mollitia at necessitatibus, ipsam quod
-          nostrum iste commodi laboriosam explicabo culpa. Enim soluta doloribus
-          repellendus, dolor magni sapiente consectetur nulla esse. Optio eos
-          impedit officia porro, voluptate, ea perspiciatis cum, dolore aut
-          facilis eum neque necessitatibus modi itaque distinctio ut totam quas.
-          Iste, ullam? Tenetur expedita dolores atque voluptate, facere tempora
-          nisi non obcaecati perspiciatis libero explicabo quidem, excepturi
-          vitae minus? Consequatur error ab odit dolor sint numquam enim vero
-          qui cum? Soluta voluptatum repellendus nemo consectetur fuga placeat
-          officia magni excepturi animi molestiae rem explicabo suscipit itaque
-          aut eligendi culpa ducimus accusamus adipisci ipsa quam, asperiores
-          labore corrupti? Pariatur rerum rem culpa ducimus quaerat at itaque
-          est, laborum obcaecati doloribus ad provident atque sit eaque eligendi
-          fugiat illum, facere delectus earum dicta inventore voluptas
-          praesentium. Ea nam mollitia explicabo iste commodi nihil blanditiis.
-          Magnam sequi numquam veritatis! Eligendi earum vel nihil. Excepturi
-          consectetur aspernatur quam ipsam assumenda fugit placeat corrupti,
-          nam cum sint quibusdam sed commodi nulla at dignissimos facilis
-          voluptatibus in porro? Quae perspiciatis quasi pariatur ullam enim
-          dolores? Repellat iste repudiandae quo id rem? Libero exercitationem
-          suscipit laboriosam commodi accusantium ducimus sit neque eveniet,
-          magnam architecto quos odio quis quisquam nemo non voluptas placeat ad
-          saepe quae molestias laudantium veniam iusto explicabo dolor. Vero
-          provident libero ratione modi. Amet id impedit omnis, harum ipsam
-          distinctio sequi sint corrupti officia animi ratione, fugiat aperiam
-          dicta assumenda ipsum quisquam sed quas exercitationem architecto!
-          Nostrum.
-        </p>
-      </div>
-    </>
+    <div className={styles.main}>
+      <h1>{blog.title}</h1>
+      <p className={styles.blogtext}>{blog.description}</p>
+    </div>
   );
 };
 
-export default slug;
+export default BlogPost;
